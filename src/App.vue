9e97c2856dda7efd9a52dbc3cbf57b6429591412
SAM <milsontw@gmail.com>
2023-06-08 17:20:56 +0800

    first stage

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
-<template lang="">
-  <div>
-    
-  </div>
-</template>
 <script>
+import { RouterView } from "vue-router"
+import HeaderView from "./components/Header.vue";
 export default {
-
+  components: {
+    HeaderView
+  },
+  data() {
+    return {
+      isShow:true
+    }
+  },
 }
 </script>
-<style lang="">
+
+    
+<template>
+  <HeaderView v-if="isShow"/>
+  <div class="wrap">
+    <RouterView />
+  </div>
+</template>
+<style>
+.wrap {
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
   
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import AddCar from '../views/addCar.vue';
+import ResearchCar from '../views/researchCar.vue';
+import DeleteCar from '../views/deleteCar.vue';
+import FinishCar from '../views/finishCar.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      // component: HomeView
+      path: '/AddCar',
+      name: 'AddCar',
+      component: AddCar
+    },
+    {
+      path: '/ResearchCar',
+      name: 'ResearchCar',
+      component: ResearchCar
+    },
+    {
+      path: '/DeleteCar',
+      name: 'DeleteCar',
+      component: DeleteCar
+    },
+    {
+      path: '/FinishCar',
+      name: 'FinishCar',
+      component: FinishCar
     }
+    
   ]
 })
 
