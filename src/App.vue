6a9ad95f2e0eb1fd7f48861a8e08d71b3b5c520a
LAPTOP-DQT65H7B\User <sha2672tlpu@gmail.com>
2023-06-09 09:03:38 +0800

    first stage

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,44 @@
 <script>
-import HeaderView from './components/Header.vue';
+// import HeaderView from './components/Header.vue';
+import LeftView from './components/Left.vue';
 
-import FooterView from './components/Footer.vue';
+// import FooterView from './components/Footer.vue';
 
-import {RouterView } from "vue-router"
+import { RouterView } from "vue-router"
 
 export default {
 
-  components:{
-    
-    HeaderView,
-    FooterView
+  components: {
+    // HeaderView,
+    LeftView,
+    // FooterView
   },
   data() {
     return {
-      
+
     }
   }
 }
 </script>
 <template>
-  
   <div class="wrap">
-    <HeaderView />
-    <RouterView />
-    <FooterView />
+    <!-- <HeaderView /> -->
+
+    <div class="left">
+      <LeftView />
+    </div>
+
+    <div class="nav">
+      <RouterView />
+    </div>
+    
+    <!-- <FooterView /> -->
   </div>
 </template>
 
 <style lang="scss" scoped>
+.wrap{
+  display: flex;
   
+}
 </style>
