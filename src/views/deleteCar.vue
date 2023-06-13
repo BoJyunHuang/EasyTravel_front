5b7708862ec55cb0de26466a362622c5724c98df
SAM <milsontw@gmail.com>
2023-06-13 14:26:05 +0800

    in the middle

--- src/views/deleteCar.vue
+++ src/views/deleteCar.vue
@@ -1,16 +1,119 @@
 <script>
+
+import TableView from "../components/Table.vue"
+import Modal from "../components/Modal.vue"
+export default {
+    components: {
+        TableView, // 帶入表格元件
+        Modal // 帶入跳出式視窗元件
+    },
+    data() {
+        return {
+            tableColumns: ['licensePlate', 'price', 'startTime', 'endTime', 'note'], // 表格標題
+            maintenanceData: [], // 表格內容
+            searchText: '', // 搜尋關鍵字
+            showEditButton: true,  // 是否顯示修改按鈕
+            showDeleteButton: true,  // 是否顯示刪除按鈕
+            isShow: false // 顯示跳出式視窗
+        };
+    },
+    mounted() { // 預設執行方法，進入頁面後隨即執行，後端-顯示所有站點資料
+        fetch("http://localhost:8080/find_all_finished_abnormal")
+            .then(res => res.json())
+            .then(data => {
+              console.log(data);
+              this.maintenanceData = data.maintenanceList})
+    },
+    methods: { // 各種方法
+        updateFilteredData() { // 更新數據內容的方法
+            if (!this.searchText) { // 對應到"搜尋內容"，若無則不改變內容
+                return this.maintenanceData;
+            }
+            const keyword = this.searchText.toLowerCase(); // 關鍵字"搜尋內容"
+            return this.maintenanceData.filter(item => // 將資料進行過濾，回傳含關鍵字資料
+                item.city.includes(keyword)
+            )
+        }, switchModal() { // 跳出式視窗顯示與否
+            this.isShow = !this.isShow
+        }
+    },
+    watch: { // 事件監聽
+        searchText: function (newText, oldText) { // 當"搜尋內容"發生改變時
+            // 在 searchText 變化時執行相應的操作
+            this.updateFilteredData();
+        }
+    },
+    computed: { // 改變顯示內容，對應到內容因應關鍵字變化的方法
+        filteredData() {
+            if (!this.searchText) {
+                return this.maintenanceData;
+            }
+            const keyword = this.searchText.toLowerCase();
+            return this.maintenanceData.filter(item => {
+                // 根據需要調整下面的條件
+                return (
+                    item.city.includes(keyword)
+                );
+            });
+        }
+    }
+}
 </script>
 
 <template>
-  <div class="top">
-  <h2>刪除維修車輛</h2>
-</div>
+    <div class="fee-manager">
+        <h2>サイト</h2>
+        <div class="d-flex justify-content-between">
+            <select class="form-select w-50 mb-2" aria-label="Default select example" v-model="searchText">
+                <option value="" disabled selected>都道府県を選ぶ</option>
+                <option value="">全ての都道府県から選ぶ</option>
+                <option value="東京都">東京都</option>
+                <option value="千葉縣">千葉縣</option>
+                <option value="神奈川縣">神奈川縣</option>
+            </select>
+            <button type="button" class="btn btn-success mb-2 px-3" @click="switchModal">サイト追加</button>
+        </div>
+        <p>※bikeAmountは自転車類の数、motorcycleAmountは二輪車の数、carAmountは四輪車の数を表しています。</p>
+        <TableView :columns="tableColumns" :data="filteredData" :showEditButton="showEditButton"
+            :showDeleteButton="showDeleteButton" />
+        <Modal v-if="isShow" @pushOutside="switchModal">
+            <H2 class="m-2">サイト追加</H2>
+            <table class="m-3 ">
+                <tr>
+                    <th><label for="city" class="my-2">都道府県</label></th>
+                    <td><input type="text" placeholder="ex:東京都" id="city"></td>
+                </tr>
+                <tr>
+                    <th><label for="location" class="my-2">ロケーション</label></th>
+                    <td><input type="text" min="0" title="会社の拠点" id="location"></td>
+                </tr>
+                <tr>
+                    <th><label for="bike_amount" class="my-2">自転車類の数</label></th>
+                    <td><input type="number" min="0" title="0以上を含む" id="bike_amount"></td>
+                </tr>
+                <tr>
+                    <th><label for="motorcycle_amount" class="my-2">二輪車の数</label></th>
+                    <td><input type="number" min="0" title="0以上を含む" id="motorcycle_amount"></td>
+                </tr>
+                <tr>
+                    <th><label for="car_amount" class="my-2">四輪車の数</label></th>
+                    <td><input type="number" min="0" title="0以上を含む" id="car_amount"></td>
+                </tr>
+            </table>
+            <div class="w-25 d-flex justify-content-between">
+                <button type="button" class="btn btn-success btn-sm px-3">決定</button>
+                <button type=" button" class="btn btn-danger btn-sm px-3" @click="switchModal">キャンセル</button>
+            </div>
+        </Modal>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+.fee-manager {
+    margin: 2rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
 
-.top{
-  margin-top: 50px;
 }
-  
 </style>
--- src/views/researchCar.vue
+++ src/views/researchCar.vue
@@ -1,44 +1,115 @@
+<template>
+  <div class="fee-manager">
+    <h2>車両修理の検索</h2>
+    <div class="d-flex justify-content-between">
+      <select class="form-select w-50 mb-2" v-model="sortOption" @change="sortData">
+        <option value="" disabled selected>近頃</option>
+        <option value="timeAsc">時間: 遠い順</option>
+        <option value="timeDesc">時間: 近い順</option>
+        <option value="priceAsc">価格: 高い順</option>
+        <option value="priceDesc">価格: 安い順</option>
+      </select>
+      
+    </div>
+    <p>※bikeAmountは自転車類の数、motorcycleAmountは二輪車の数、carAmountは四輪車の数を表しています。</p>
+    <TableView :columns="tableColumns" :data="formattedData" :showEditButton="showEditButton"
+      :showDeleteButton="showDeleteButton" />
+   
+  </div>
+</template>
+
 <script>
+import TableView from "../components/Table.vue";
+import Modal from "../components/Modal.vue";
+
 export default {
+  components: {
+    TableView,
+    Modal
+  },
   data() {
     return {
-      text: null,
-      showTable: false,
-      selectedOption: "",
+      tableColumns: ['licensePlate', 'price', 'startTime', 'endTime', 'note'],
+      maintenanceData: [],
+      searchText: '',
+      showEditButton: true,
+      showDeleteButton: true,
+      isShow: false,
+      sortOption: 'timeDesc' // 預設以時間降序排序
     };
   },
+  mounted() {
+    fetch("http://localhost:8080/find_all_finished_abnormal")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.maintenanceData = data.maintenanceList.map(item => {
+          return {
+            ...item,
+            startTime: new Date(item.startTime).toLocaleString('ja-JP'),
+            endTime: new Date(item.endTime).toLocaleString('ja-JP')
+          };
+        });
+      });
+  },
   methods: {
-    showTableOnClick() {
-      this.showTable = true;
+    updateFilteredData() {
+      if (!this.searchText) {
+        return this.maintenanceData;
+      }
+      const keyword = this.searchText.toLowerCase();
+      return this.maintenanceData.filter(item =>
+        item.city.includes(keyword)
+      );
     },
-    
+    switchModal() {
+      this.isShow = !this.isShow;
+    },
+    sortData() {
+      if (this.sortOption === 'timeAsc') {
+        this.filteredData.sort((a, b) => new Date(a.startTime) - new Date(b.startTime)); // 按開始時間升序排序
+      } else if (this.sortOption === 'timeDesc') {
+        this.filteredData.sort((a, b) => new Date(b.startTime) - new Date(a.startTime)); // 按開始時間降序排序
+      } else if (this.sortOption === 'priceAsc') {
+        this.filteredData.sort((a, b) => a.price - b.price); // 按價格升序排序
+      } else if (this.sortOption === 'priceDesc') {
+        this.filteredData.sort((a, b) => b.price - a.price); // 按價格降序排序
+      }
+    }
+  },
+  watch: {
+    searchText() {
+      this.updateFilteredData();
+    }
+  },
+  computed: {
+    filteredData() {
+      if (!this.searchText) {
+        return this.maintenanceData;
+      }
+      const keyword = this.searchText.toLowerCase();
+      return this.maintenanceData.filter(item =>
+        item.city.includes(keyword)
+      );
+    },
+    formattedData() {
+      return this.filteredData.map(item => {
+        return {
+          ...item,
+          startTime: new Date(item.startTime).toLocaleString('ja-JP'),
+          endTime: new Date(item.endTime).toLocaleString('ja-JP')
+        };
+      });
+    }
   }
 };
 </script>
 
-<template>
-  <div class="top">
-  <h2>修理車両の検索</h2>
-</div>
-
-  <div v-if="!showTable" class="mider">
-      <p>未完成の車牌検索を入力してください</p>
-      <input type="text" v-model="text">
-      <button type="button" class="btn" @click="showTableOnClick">確認</button>
-    </div>
-
-</template>
-
 <style lang="scss" scoped>
-
-.top{
-  margin-top: 50px;
-}
-.mider {
+.fee-manager {
+  margin: 2rem;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  margin-top: 150px;
+  justify-content: left;
 }
-  
 </style>
