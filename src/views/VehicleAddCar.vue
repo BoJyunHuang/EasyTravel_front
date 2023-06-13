9da09b95393608b852f51ae001c6dce609029027
LAPTOP-LO6OLGEL\ASUS <sakura1010435@gmail.com>
2023-06-13 22:58:48 +0800

    finished vehicle fetch

--- src/views/VehicleAddCar.vue
+++ src/views/VehicleAddCar.vue
@@ -1,29 +1,42 @@
 <script>
 import Modal from "../components/Modal.vue"
 import TableView from "../components/Table.vue"
+import MessageModal from "../components/messageModal.vue"
 export default {
   components: {
     Modal,
-    TableView
+    TableView,
+    MessageModal
   },
   data() {
     return {
       vehicleColumn: [`licensePlate`, `category`, `cc`, `startServingDate`, `latestCheckDate`, `available`, `city`, `location`, `odo`, `price`],
       vehicleData: [],
+      deleteData: [],    // 刪除車資料
       // table button 編輯按鈕
-      showEditButton: true,
-      showDeleteButton: true,
+      showEditButton: true,   // 顯示變更按鈕
+      showDeleteButton: true, // 顯示刪除按鈕
+      showCompleteButton: false,
+      showControl: true,      // 顯示操作行
+      item: {},               // 直接編輯資料
       // modal
-      isAddCarShow: false,
-      isScrapCarShow: false,
-      isUpdateCarShow: false,
-      modalTitle: null,
+      isAddCarShow: false,    // 顯示新增視窗
+      isScrapCarShow: false,  // 顯示報廢視窗
+      isUpdateCarShow: false, // 顯示變更視窗
+      isMessage: false,       // 顯示回覆視窗
+      message: ``,            // 後端回傳訊息
+
       ccRange: "bike : 0\nscooter : 1~250\nmotorcycle : 251~550\nheavy motorcycle : >550\nsedan/ven/suv : 自定義(1200~6600)",
       // fetch / find by category
-      carInfo: document.getElementById("carInfo"),
       categoryInput: document.getElementById("categoryInput"),
       // fetch / add car
-      plateNumInput
+      plateNumInput: '',
+      categoryInput: '',
+      tankInput: '',
+      priceInput: '',
+      // fetch / update car
+      available: true,
+      reqOdo: 0
 
     }
   },
@@ -55,10 +68,89 @@ export default {
         console.log(data)
         if(data.message == "Success!"){
           this.vehicleData = data.vehicleList
+        }else {
+          this.Reload()
         }
       })
-    }
+    },
+    // add car
+    addCar() {
+      let body = {
+        "licensePlate" : this.plateNumInput,
+        "category" : this.categoryInput,
+        "cc" : this.tankInput,
+        "price" : this.priceInput
+      }
+      console.log("add_click")
+      fetch("http://localhost:8080/add_car", {
+        method: "POST",
+        headers: {
+          "Content-type" : "application/json"
+        },
+        body: JSON.stringify(body)
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.message = data.message
+        this.isMessage = true
+      })
+    },
+    // update car
+    // 顯示更改視窗
+    updateCar(item) {
+      this.isUpdateCarShow = !this.isUpdateCarShow
+      this.item = item
+    },
+    // 執行方法
+    finalUpdateCar() {
+      let body = {
+        "licensePlate" : this.item.licensePlate,
+        "odo" : this.reqOdo,
+        "available" : this.available
 
+      }
+      console.log("click")
+      fetch("http://localhost:8080/update_car_info", {
+        method: "POST",
+        headers: {
+          "Content-type" : "application/json"
+        },
+        body: JSON.stringify(body)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.message = data.message
+        this.isMessage = true
+      })
+    },
+    // 報廢車
+    scrapCar(item) {
+      this.item = item
+      let body = {
+        "licensePlate" : this.item.licensePlate
+      }
+      console.log(body)
+      fetch("http://localhost:8080/scrap_car", {
+        method: "POST",
+        headers: {
+          "Content-type" : "application/json"
+        },
+        body: JSON.stringify(body)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.message = data.message
+        this.isMessage = true
+      })
+    },
+    Reload() {
+      this.isAddCarShow = false
+      this.isScrapCarShow = false
+      this.isUpdateCarShow = false
+      window.location.reload()
+    }
 
 
   },
@@ -69,6 +161,13 @@ export default {
     .then(data => {
       console.log(data.vehicleList)
       this.vehicleData = data.vehicleList
+    }),
+    // 找報廢
+    fetch("http://localhost:8080/find_car_near_scrap")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.vehicleList)
+      this.deleteData = data.vehicleList
     })
     
   }
@@ -103,8 +202,8 @@ export default {
 
     <!-- table -->
     <div class="carInfoTable">
-      <TableView :columns="vehicleColumn" :data="vehicleData" :showEditButton="showEditButton"
-            :showDeleteButton="showDeleteButton" />
+      <TableView :columns="vehicleColumn" :data="vehicleData" :showControl="showControl" :showEditButton="showEditButton"
+            @edit="updateCar" />
     </div>
 
     <!-- Modal -->
@@ -114,12 +213,12 @@ export default {
       <table class="h-50">
         <tr class="my-2">
           <th>車両番号</th>
-          <td><input type="text" id="plateNumInput"></td>
+          <td><input type="text" v-model="plateNumInput"></td>
         </tr>
         <tr class="my-2">
           <th>車種</th>
           <td>
-            <select name="車種" id="categoryInput">
+            <select name="車種" v-model="categoryInput">
               <option value="bike">bike</option>
               <option value="scooter">scooter</option>
               <option value="motorcycle">motorcycle</option>
@@ -132,44 +231,88 @@ export default {
         </tr>
         <tr class="my-2">
           <th :title="ccRange">タンク容量</th>
-          <td><input type="number" id="tankInput" :title="ccRange"></td>
+          <td><input type="number" v-model="tankInput" :title="ccRange"></td>
         </tr>
         <tr class="my-2">
           <th>値段</th>
-          <td><input type="number" id="priceInput"></td>
+          <td><input type="number" v-model="priceInput"></td>
         </tr>
       </table>
 
-      <button class="workBtn btn btn-success mt-2 ">追加</button>
+      <button type="button" class="workBtn btn btn-success mt-2" @click="addCar">追加</button>
 
     </Modal>
 
     <!-- Scrap Car -->
     <Modal v-if="isScrapCarShow" @pushOutside="switchScrapCar">
       <h2 class="mt-4 text-vehicle fw-bold">車の廃棄</h2>
-      <table class="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col">車両番号</th>
-            <th scope="col">車種</th>
-            <th scope="col">追加日</th>
-            <th scope="col">最新検査日</th>
-            <th scope="col">ステータス</th>
-            <th scope="col">走行距離</th>
-            <!-- <th scope="col">値段</th> -->
-            <th scope="col">廃棄</th>
-          </tr>
-        </thead>
-        
-        <tbody id="scrapCarInfo">
-        </tbody>
-      </table>
+      <TableView :columns="vehicleColumn" :data="deleteData" :showControl="showControl" :showDeleteButton="showDeleteButton" @delete="scrapCar">
+
+      </TableView>
     </Modal>
 
     <!-- Update Car -->
     <Modal v-if="isUpdateCarShow" @pushOutside="switchUpdateCar">
       <h2 class="mt-4 text-vehicle fw-bold">車の情報変更</h2>
+      <table class="h-50">
+        <tr class="my-2">
+          <th>車両番号</th>
+          <td>{{ item.licensePlate }}</td>
+        </tr>
+        <tr class="my-2">
+          <th>車種</th>
+          <td>{{ item.category }}</td>
+        </tr>
+        <tr class="my-2">
+          <th>タンク容量</th>
+          <td>{{ item.cc }}</td>
+        </tr>
+        <tr class="my-2">
+          <th>追加日</th>
+          <td>{{ item.startServingDate }}</td>
+        </tr>
+        <tr class="my-2">
+          <th>最新検査日</th>
+          <td>{{ item.latestCheckDate }}</td>
+        </tr>
+        <tr class="my-2">
+          <th>ステータス</th>
+          <td><select name="available" id="available" v-model="available">
+              <option value="true">true</option>
+              <option value="false">false</option>
+              </select>
+          </td>
+        </tr>
+        <tr class="my-2">
+          <th>エリア</th>
+          <td>{{ item.city }}</td>
+        </tr>
+        <tr class="my-2">
+          <th>ポート</th>
+          <td>{{ item.location }}</td>
+        </tr>
+        <tr class="my-2">
+          <th>走行距離</th>
+          <td>{{ item.odo }}
+            <span> + </span>
+            <input type="number" v-model="reqOdo">
+            <span>キロ</span>
+          </td>
+        </tr>
+        <tr class="my-2">
+          <th>値段</th>
+          <td>{{ item.price }}</td>
+        </tr>
+      </table>
+
+      <button type="button" class="workBtn btn btn-success my-2" @click="finalUpdateCar">変更</button>
+
     </Modal>
+
+
+    <MessageModal v-if="isMessage" @getReady="Reload">
+      <p>{{ message }}</p>
+    </MessageModal>
 
   </div>
 </template>
