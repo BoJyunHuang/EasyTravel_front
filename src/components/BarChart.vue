4c60ee7510f6ca695cbe33ce2d12fc49d260f2dc
Jyun <taiwanman1212@gmail.com>
2023-06-26 14:34:10 +0800

    barchart complete

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,76 +1,64 @@
+<template>
+    <Bar :data="chartData" :options="chartOptions" v-if="chartData.labels.length > 0" />
+</template>
+  
 <script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
 export default {
-    props: [
-        "data", // 全部資料
-        "title",
-    ],
+    name: 'BarChart',
+    components: { Bar },
+    props: {
+        label: "",
+        labels: [],
+        data: {},
+    },
+    computed: {
+        chartData() {
+            return {
+                labels: this.labels,
+                datasets: [
+                    {
+                        label: this.label,
+                        backgroundColor: '#4FC3F7',
+                        data: this.data,
+                    },
+                ],
+            };
+        },
+    },
     data() {
         return {
-            chart: null,
-            timeout: null,
-            options: {
-                title: {
-                    text: "財務"
+            chartOptions: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        ticks: {
+                            callback: (value) => `${value / 10000} 万円`,
+                        },
+                    },
                 },
-                axisY: {
-                    title: "金額　（百万）",
-                    suffix: "￥",
-                    minimum: 0,
-                    stripLines: [{ // 設定基準線
-                        value: '', // 線名稱
-                        labelAlign: "near",
-                        label: "", // 顯示標籤
-                        showOnTop: true,
-                        color: "#335E28",
-                        labelFontColor: "#335E28"
-                    }]
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: (context) => `${context.parsed.y / 10000} 万円`,
+                        },
+                    },
                 },
-                data: [{
-                    type: "column",
-                    yValueFormatString: "#,##0",
-                    indexLabel: "{y}",
-                    dataPoints: [
-                        { label: "Furnace 1", y: 1390 },
-                        { label: "Furnace 2", y: 1300 },
-                        { label: "Furnace 3", y: 1560 },
-                        { label: "Furnace 4", y: 1206 },
-                        { label: "Furnace 5", y: 1400 },
-                        { label: "Furnace 6", y: 1050 }
-                    ]
-                }]
-            },
-            styleOptions: {
-                width: "100%",
-                height: "360px"
             }
         }
     },
-    methods: {
-        updateData() {
-            let boilerColor, deltaY, yVal;
-            let dps = this.chart.options.data[0].dataPoints;
-            for (let i = 0; i < dps.length; i++) {  // 更新資料部分
-                deltaY = Math.round(20 + Math.random() * (-20 - 20));
-                yVal = Math.max(deltaY + dps[i].y, 700);
-                boilerColor = yVal > 1500 ? "#FF7043" : yVal >= 1100 ? "#81C784" : "#42A5F5";
-                dps[i] = { label: "Furnace " + (i + 1), y: yVal, color: boilerColor };
-            }
-            this.options.data[0].dataPoints = dps;
-            this.chart.render(); // 重新生成圖表
-
-            this.timeout = setTimeout(this.updateData, 1000); // 設定更新時間
+    watch: {
+        labels(newLabels) {
+            this.chartData.labels = newLabels;
         },
-        chartInstance(chart) {
-            this.chart = chart;
-            this.updateData();
-        }
+        data(newData) {
+            this.chartData.datasets[0].data = newData;
+        },
     },
-    unmounted() {
-        clearTimeout(this.timeout); // 清除時間
-    }
 }
 </script>
-  
-<template>
-    <CanvasJSChart :options="options" :style="styleOptions" @chart-ref="chartInstance" />
-</template>    
--- src/views/FinanceChart.vue
+++ src/views/FinanceChart.vue
@@ -7,72 +7,71 @@ export default {
         Doughnut
     }, data() {
         return {
-            sliderValue: 1,
-            financeDate: {},
+            selectedMonth: 1,
+            months: [
+                { value: 1, label: '一月' },
+                { value: 2, label: '二月' },
+                { value: 3, label: '三月' },
+                { value: 4, label: '四月' },
+                { value: 5, label: '五月' },
+                { value: 6, label: '六月' },
+            ],
             financeRatio: {},
 
-            iTitle: ["vip_income", "rent_income_ratio"],
-            iDetail: ["rent_income"],
-            eTitle: ["vehicle_cost_ratio", "maintenance_cost_ratio"],
-            eDetail: ["vehicle_cost", "maintenance_cost"]
+            riKeys: [],
+            riValues: [],
         };
     },
     mounted() { // 預設執行方法，進入頁面後隨即執行，後端-顯示所有站點資料
-        let body = {
-            "month": 1
-        }
-        fetch("http://localhost:8080/monthly_report", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(body)
-        }).then(res => res.json())
-            .then(data => this.financeDate = data.financeMap)
-        fetch("http://localhost:8080/monthly_ratio", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(body)
-        }).then(res => res.json())
-            .then(data => {
-                this.financeRatio = data.financeMap
-            })
+        this.updateData(); // 初始获取数据
+        // fetch("http://localhost:8080/monthly_ratio", {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: JSON.stringify(body)
+        // }).then(res => res.json())
+        //     .then(data => {
+        //         this.financeRatio = data.financeMap
+        //     })
     },
-    // methods: { // 各種方法
-    //     updateData() { // 更新數據內容的方法
-    //         const body = {
-    //             month: this.sliderValue
-    //         }
-    //         fetch("http://localhost:8080/monthly_report", {
-    //             method: "POST",
-    //             headers: {
-    //                 "Content-Type": "application/json"
-    //             },
-    //             body: JSON.stringify(body)
-    //         }).then(res => res.json())
-    //             .then(data => this.financeDate = data.financeMap)
-    //         fetch("http://localhost:8080/monthly_ratio", {
-    //             method: "POST",
-    //             headers: {
-    //                 "Content-Type": "application/json"
-    //             },
-    //             body: JSON.stringify(body)
-    //         }).then(res => res.json())
-    //             .then(data => this.financeRatio = data.financeMap)
-    //     }
-    // },
+    methods: { // 各種方法
+        updateData() { // 更新數據內容的方法
+            const body = {
+                month: this.selectedMonth
+            }
+            fetch("http://localhost:8080/monthly_report", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            }).then(res => res.json())
+                .then(data => {
+                    this.riKeys = Object.keys(data.financeMap.rent_income);
+                    this.riValues = Object.values(data.financeMap.rent_income);
+                })
+            // fetch("http://localhost:8080/monthly_ratio", {
+            //     method: "POST",
+            //     headers: {
+            //         "Content-Type": "application/json"
+            //     },
+            //     body: JSON.stringify(body)
+            // }).then(res => res.json())
+            //     .then(data => this.financeRatio = data.financeMap)
+        }
+    },
 };
 </script>
-
 <template>
     <div class="w-100 p-3">
         <h2>データ整理</h2>
-        <input class="movebar" type="range" id="rangeInput" v-model="sliderValue" min="1" max="5" step="1">
-        <span class="mx-3">{{ sliderValue }}</span>
-        <label for="rangeInput">月</label>
-        <BarChart />
+        <div>
+            <select v-model="selectedMonth" @change="updateData">
+                <option v-for="month in months" :key="month.value" :value="month.value">{{ month.label }}</option>
+            </select>
+        </div>
+        <BarChart :label="'rent_income'" :labels="riKeys" :data="riValues" />
         <Doughnut />
     </div>
 </template>
